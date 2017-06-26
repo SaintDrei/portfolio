3d1cacf5bb3d8b5cc5964e4e3b50fa79c1da8462
busio <busio>
2017-06-26 10:33:17 +0800

    Live repository pulled from host

--- content/script.js
+++ content/script.js
@@ -5,5 +5,9 @@ $(document).ready(function() {
        $(".homenav").click(function() {
   
       //     $('html,body').animate({        scrollTop: $("#home").offset().top},         slow');       });
-    
+jQuery('.skillbar').each(function(){
+		jQuery(this).find('.skillbar-bar').animate({
+			width:jQuery(this).attr('data-percent')
+		},6000);
+	});   
 });
